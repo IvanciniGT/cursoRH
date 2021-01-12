e66097f2746f4ca65e359e8a2486b625cdf0ad1a
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2021-01-12 11:46:10 +0100

    Update installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -1,7 +1,6 @@
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf repolist -v
 dnf list docker-ce --showduplicates | sort -r
-sudo dnf install --nobest docker-ce
 sudo yum remove docker
 sudo yum remove podman
 sudo dnf install --nobest --allowerasing docker-ce
